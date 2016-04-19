76002c60370164a2961432c6dcc69b6c91fac805
Souloist <rich.shen@nyu.edu>
2016-04-19 12:43:38 -0400

    added generic

--- lists/SinglyLinkedList.java
+++ lists/SinglyLinkedList.java
@@ -1,5 +1,17 @@
-public class SinglyLinkedList{
-	private IntNode sentinel;
+public class SinglyLinkedList<T>{
+
+	public class Node{
+
+		public T item;
+		public Node next;
+
+		public Node(T i, Node n){
+			item = i;
+			next = n;
+		}
+	}	
+
+	private Node sentinel;
 	private int size;
 
 	/* Constructor for emptry sList */
@@ -8,35 +20,35 @@ public class SinglyLinkedList{
 		/* The value of sentinel does not matter
 		 * What matters is that the value is not null
 		 * This allows us to avoid nullpointer exceptions */
-		sentinel = new IntNode(232, null);
+		sentinel = new Node(null, null);
 	}
 
 	/* Constructor for sList of size 1 */
-	public SinglyLinkedList(int x){
+	public SinglyLinkedList(T x){
 		size = 1;
-		sentinel = new IntNode(232, null);
-		sentinel.next = new IntNode(x, null);
+		sentinel = new Node(null, null);
+		sentinel.next = new Node(x, null);
 	}
 
 	/* Inserts new node to front of sList */
-	public void insertFront(int x){
+	public void insertFront(T x){
 
 		/* Remember oldFrontNode is not the actual front node
 		 * It is the node behind the sentinal node */
-		IntNode oldFrontNode = sentinel.next;
-		sentinel.next = new IntNode(x, oldFrontNode);
+		Node oldFrontNode = sentinel.next;
+		sentinel.next = new Node(x, oldFrontNode);
 		size++;
 	}
 
 	/* Returns first value of sList */
-	public int getFront(){
+	public T getFront(){
 		return sentinel.next.item;
 	}	
 
 	/* Helper method to get the last node */
 
-	private IntNode getBackNode(){
-		IntNode p = sentinel;
+	private Node getBackNode(){
+		Node p = sentinel;
 
 		while (p.next != null){
 			p = p.next;
@@ -45,17 +57,17 @@ public class SinglyLinkedList{
 		return p;
 	}
 	/* Insets new node to back of sList */
-	public void insertBack(int x){
+	public void insertBack(T x){
 
-		IntNode oldback = getBackNode();
-		oldback.next = new IntNode(x, null);
+		Node oldback = getBackNode();
+		oldback.next = new Node(x, null);
 		size++;
 	}
 
 	/*Returns last value of sList */
-	public int getBack(){
+	public T getBack(){
 
-		IntNode oldback = getBackNode();
+		Node oldback = getBackNode();
 		return oldback.item;
 	}	
 
@@ -65,26 +77,17 @@ public class SinglyLinkedList{
 	}
 
 	public static void main(String[] args){
-		SinglyLinkedList s1 = new SinglyLinkedList();
+		SinglyLinkedList<Integer> s1 = new SinglyLinkedList<Integer>();
 		s1.insertBack(3);	
 		s1.insertFront(5);
-		s1.insertFront(3);
 
+		SinglyLinkedList<String> s2 = new SinglyLinkedList<String>();
+		s2.insertFront("HI");
 
 		// 3 -> 5 -> 3
 
-		System.out.println("Last value in sList: " + s1.getBack());
-		System.out.println("Size of sList is: " + s1.size());
-	}
-}
-
-class IntNode{
-
-	public int item;
-	public IntNode next;
-
-	public IntNode(int i, IntNode n){
-		item = i;
-		next = n;
+		System.out.println("Last value in s1: " + s1.getBack());
+		System.out.println("Size of s1 is: " + s1.size());
+		System.out.println("Last value in s2 is: " + s2.getBack());
 	}
 }
