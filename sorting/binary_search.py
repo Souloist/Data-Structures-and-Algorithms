9bee239a13a7351572ab8d6178feb437c9dc5559
Souloist <rich.shen@nyu.edu>
2016-04-24 17:47:00 -0400

    updated

---
+++ sorting/binary_search.py
@@ -0,0 +1,25 @@
+''' 
+Performs binary search an an array and returns False if the
+number does not exist in the array, otherwise, it returns the
+index of the number
+'''
+
+def binary_search(value, array, min, max):
+
+	mid = (min+max)/2
+
+	if array[mid] != value and (max-min) < 2:
+		return False
+
+	if (value < array[mid]):
+		return binary_search(value, array, min, mid - 1)
+	elif (value > array[mid]):
+		return binary_search(value, array, mid + 1, max)
+	else:
+		return mid
+
+# test
+array = [1, 3, 25, 223, 500, 21341, 29410]
+
+print binary_search(25, array, 0, 7)
+print binary_search(24, array, 0, 7)
--- sorting/bubble_sort.py
+++ sorting/bubble_sort.py
@@ -1,7 +1,30 @@
-def bubbleSort(alist):
-    for passnum in range(len(alist)-1,0,-1):
-        for i in range(passnum):
-            if alist[i]>alist[i+1]:
-                temp = alist[i]
-                alist[i] = alist[i+1]
-                alist[i+1] = temp
+''' 
+bubbleSort sorts an array by iterating through and swapping consecutive pairs of numbers if they are out of over.
+This algorithm will take O(n^2) time and O(n) space.
+'''
+
+def bubbleSort(array):
+
+ 	for i in reversed(range(len(array))):
+ 		
+ 		swaps = 0
+		
+		for j in range(i):
+			if array[j] > array[j+1]:
+				temp = array[j+1]
+				array[j+1] = array[j]
+				array[j] = temp
+				swaps += 1
+		
+		if swaps == 0:
+			return
+
+
+# test
+array = [1, 2, 3, 6, 3,2, 6, 3, 2, 1]
+
+print "Unsorted: " + str(array)
+
+bubbleSort(array)
+
+print "Sorted:   " + str(array)
